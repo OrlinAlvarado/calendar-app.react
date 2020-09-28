f2b4af4997383a360aa1c6bc7ea3791da24ce89d
Orlin Alvarado <oalvaradomorales@gmail.com>
2020-09-27 22:06:25 -0600

    Publicando nuevamente para incluir home page

---
+++ docs/precache-manifest.3be0c9d6ab1492fe4bcface58e9da9d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88127b6d34c0e585838b7cb4cb85926b",
+    "url": "/calendar-app.react/index.html"
+  },
+  {
+    "revision": "bcf19e43f4c2af28ac1d",
+    "url": "/calendar-app.react/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "139e8c52e140ed8b5948",
+    "url": "/calendar-app.react/static/css/main.9abccf85.chunk.css"
+  },
+  {
+    "revision": "bcf19e43f4c2af28ac1d",
+    "url": "/calendar-app.react/static/js/2.2934bec9.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/calendar-app.react/static/js/2.2934bec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "139e8c52e140ed8b5948",
+    "url": "/calendar-app.react/static/js/main.a5b36560.chunk.js"
+  },
+  {
+    "revision": "8b089e4c2d9c7407dde6",
+    "url": "/calendar-app.react/static/js/runtime-main.9a91c0a0.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.98f79060c21cbfe46fa3e788add97d0b.js"
+  "/calendar-app.react/precache-manifest.3be0c9d6ab1492fe4bcface58e9da9d7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calendar-app.react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
